991c811bd5a87fd255c2ab63fd25fe040f74aa61
MadisMaisalu <madismaisalumm@gmail.com>
2022-05-18 22:38:28 +0300

    Base files

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div class="position-absolute top-50 start-50 translate-middle">
+        </div>
+    </div>
+  );
+}
+
+export default App;
