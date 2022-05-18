9c2ee78945c864dfd50bde1d7a4a19bc256e6836
MadisMaisalu <madismaisalumm@gmail.com>
2022-05-18 23:48:45 +0300

    I can see the form and enter numbers

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-        <div class="position-absolute top-50 start-50 translate-middle">
+      <div className="container-sm" style={{width: '50%', marginTop: '20%'}}>
+        <div className="row">
+            <input className="form-control" id="firstNumber" placeholder="Enter first number" />
         </div>
-    </div>
+        <div className="row" style={{marginTop: '1%', fontSize: '2rem'}}>
+            <p className="text-center">+</p>
+        </div>
+        <div className="row">
+            <input className="form-control" id="secondNumber" placeholder="Enter second number" />
+        </div>
+          <div className="row" style={{marginTop: '5%'}}>
+              <button type="button" className="btn btn-primary">=</button>
+          </div>
+          <div className="row">
+              <input className="form-control" id="secondNumber" placeholder="Answer" disabled/>
+          </div>
+      </div>
   );
 }
 
